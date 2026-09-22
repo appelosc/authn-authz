31b8ba6cf29ae40c5bdb9bb996aa02181c21ace7
appelosc <appelosc@arcada.fi>
2026-09-22 11:23:54 +0300

    project scaffolding added.

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+
+console.log(`Node.js ${process.version}`)
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ msg: "Docker works" })
+})
+
+app.listen(PORT, () => {
+    try {
+        console.log(`Running on http://localhost:${PORT}`)
+    } catch (error) {
+        console.error(`Error starting server: ${error.message}`)
+    }
+    
+})
